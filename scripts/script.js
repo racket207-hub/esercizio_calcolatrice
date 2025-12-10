80dec5b4affa19be7ffa11fd83f64d8f23d65686
racket207-hub <rocco.lore@branchina.edu.it>
2025-12-10 23:54:36 +0100

    started working on the click function for the buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,27 @@
+//fare uno switch case per ogni bottone --idea
 
-while(true){
-    for (let i = 0; i <= 9; i++){
-        document.getElementById('b' + i).addEventListener('click', () => {
-            document.getElementById('risultato').innerText = document.getElementById('b' + i).innerHTML;
-        })
-    }
+/* var buttons = document.getElementsByClassName('calculator-btn');
+var risulato_display = document.getElementById('risultato');
+
+document.getElementsByClassName('buttons').addEventListener('click', () => {
+    risultato_display.innerText = buttons.innerHTML;
+}) */
+
+/* const wrapper = document.getElementById('griglia-tasti');
+
+wrapper.addEventListener('click', (event) => {
+
+}) */
+
+// Source - https://stackoverflow.com/a
+// Posted by Scott Strauss, modified by community. See post 'Timeline' for change history
+// Retrieved 2025-12-10, License - CC BY-SA 4.0
+
+var buttons = document.querySelectorAll(".calculator-btn").length;
+var risulato_display = document.getElementById('risultato');
+
+for (var i = 0; i < buttons ; i++) {
+    document.querySelectorAll(".calculator-btn")[i].addEventListener("click", function() {
+        risulato_display.innerText = this.innerHTML
+    });
 }
