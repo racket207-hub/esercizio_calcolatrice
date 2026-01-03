7796b2bf2e435f26ec78351adec1a0ee28ed7082
racket207-hub <rocco.lore@branchina.edu.it>
2026-01-03 13:05:34 +0100

    added resizable component to the calculator depending on the screen width and modified the css to add a dark and light mode

--- android/app/src/main/java/com/rocco/app/MainActivity.java
+++ android/app/src/main/java/com/rocco/app/MainActivity.java
@@ -1,5 +1,22 @@
 package com.rocco.app;
 
 import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Grab WebView
+        WebView webView = (WebView) findViewById(com.getcapacitor.android.R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+
+        // The Magic
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+    }
+}
